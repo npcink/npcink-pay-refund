5698cac5ee6f7fb3846624610515211e06ea65b2
Muze-mac <13581261474@163.com>
2023-07-26 17:09:53 +0800

    可用

--- admin/partials/mare-admin-interface.php
+++ admin/partials/mare-admin-interface.php
@@ -59,38 +59,37 @@ if (!class_exists('Mare_Admin_Interface')) {
         //保存Option
         public static function update_option_by_RestAPI($data)
         {
-            //判断是否是管理员
-            if (current_user_can('manage_options')) {
-
-                $dataArray = json_decode($data->get_body());
-                $result = new stdClass();
-
-                //循环保存选项
-                foreach ($dataArray as $option_name => $value) {
-
-                    //判断，是否是非空数组
-                    if (is_object($value)) {
-                        //是非空数组
-                        foreach ($value as $arr => $data) {
-                            update_option($arr, $data);
-                        }
-                    } else {
-                        update_option($option_name, $value);
-                    }
-                    $result->$option_name = $value;
-                }
-
-                //返回成功信息
-                //返回成功信息
-                return new WP_REST_Response(array(
-                    'success' => true,
-                    'message' => "已保存！",
-                    'data' => $result,
-                ), 200);
-            } else {
-                //返回失败信息
-                return new WP_Error('save_error', '保存失败！', array('status' => 500));
+            // 判断是否是管理员
+            if (!current_user_can('manage_options')) {
+                // 返回失败信息
+                return new WP_Error('save_error', '保存失败！非管理员无法保存', array('status' => 500));
             }
+
+            $dataArray = json_decode($data->get_body());
+            $result = new stdClass();
+
+            //循环保存选项
+            foreach ($dataArray as $option_name => $value) {
+
+                //判断，是否是非空数组
+                if (is_object($value)) {
+                    //是非空数组
+                    foreach ($value as $arr => $data) {
+                        update_option($arr, $data);
+                    }
+                } else {
+                    update_option($option_name, $value);
+                }
+                $result->$option_name = $value;
+            }
+
+            //返回成功信息
+            //返回成功信息
+            return new WP_REST_Response(array(
+                'success' => true,
+                'message' => "已保存！",
+                'data' => $result,
+            ), 200);
         }
     } //end
 }
