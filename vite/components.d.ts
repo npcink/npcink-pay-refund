531158cf5ab68a07e11f63e94d753cf841981126
Muze-mac <13581261474@163.com>
2023-07-29 10:01:22 +0800

    调整加载

--- vite/components.d.ts
+++ vite/components.d.ts
@@ -16,7 +16,6 @@ declare module 'vue' {
     ElOption: typeof import('element-plus/es')['ElOption']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSelect: typeof import('element-plus/es')['ElSelect']
-    ElSpace: typeof import('element-plus/es')['ElSpace']
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
